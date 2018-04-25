c3db7dde6de22458e825adb0a16caf5651874185
nilokillian <nilokillian@gmail.com>
2018-04-25 22:55:52 +1200

    new bla bak

--- MenuUserControl.js
+++ MenuUserControl.js
@@ -2,7 +2,7 @@
  
  var requestUri = _spPageContextInfo.webAbsoluteUrl + "/_api/SP.UserProfiles.PeopleManager/GetMyProperties"
  var requestHeaders = {"accept" : "application/json;odata=verbose" };
-
+ var arrayAssociation;
     $.ajax({
         url : requestUri,
         contentType : "application/json;odata=verbose",
@@ -10,112 +10,46 @@
         success : onSuccess,
         error : onError
     });
+/////////////////////////
+
+var test;
 
  function onSuccess(data, request){
-
       var properties = data.d.UserProfileProperties.results;
-       for (var i = 0; i < properties.length; i++){
+           for (var i = 0; i < properties.length; i++){
               var property = properties[i];
-              if (property.Key == "Association") {
-                  var arrayAssociation = property.Value.split("|");
+              if (property.Key == "Association"){
+                  arrayAssociation = property.Value.split("|");
+                  getMenuNodes();
                  //console.log(arrayAssociation)
-                var topMenuDropNode = $('.static.dynamic-children.ms-navedit-dropNode');
+                }
+            }
+    }
 
-
-
-                $(topMenuDropNode).each(function () {
-
-                    var dropNodeTitle = $(this).attr('title');
-
-
-
-                    if ( dropNodeTitle == "GPs" ){
+  function getMenuNodes(){
+        $('ul#zz12_RootAspMenu.root.ms-core-listMenu-root.static').find('li').each(function(){
+           $(this).find('.static.dynamic-children').each(function(){ 
+                if ($(this).attr('title') == "GPs"){ 
+                    $('a.dynamic.menu-item.ms-core-listMenu-item.ms-displayInline.ms-navedit-linkNode').each(function(){
+                    //console.log($(this).attr('title'));
+                    $(this).each(function(){
+                        var curentLi = ($(this).attr('title') || '').toString();
+                        var found = !!arrayAssociation.find(function(arrayTitle){
+                        return (arrayTitle.toString()) == curentLi.toString();   
+                        });       
+                 
+                            if (found == false){
+                               //$(this).remove();
+                                console.log(curentLi + " Has been removed");
+                            } 
 
                         
-
-                       var fullLink = $(this).children('ul.dynamic a.dynamic.menu-item.ms-core-listMenu-item.ms-displayInline.ms-navedit-linkNode');
-
-
-
-                        console.log(fullLink)
-
-
-
-                    }
-
-
-
-                    
-
-                });
-
-
-
-
-
-               // var topMenulinkNode =  $('a.static.dynamic-children.ms-navedit-dropNode.menu-item.ms-core-listMenu-item.ms-displayInline.ms-navedit-linkNode')
-
-               // $(topMenulinkNode).each(function () {
-
-
-
-               //        console.log($(this).attr('title'))
-
-              //  });
-
-             
-
-                  var dynamicMenu = $('span.menu-item-text ul.dynamic a.dynamic.menu-item.ms-core-listMenu-item.ms-displayInline.ms-navedit-linkNode')
-
-                  
-
-              
-
-                
-
-                  $(dynamicMenu).each(function () {
-
-  
-
-                      var curentLi = ($(this).attr('title') || '').toString();
-
-
-
-                      var found = !!arrayAssociation.find(function(arrayTitle) {
-
-                          return (arrayTitle.toString()) == curentLi.toString();   
-
-                      });
-
-                        if (found == false)
-
-                        {
-
-                          $(this).remove();
-
-                          console.log(curentLi + " Has been removed");
-
-                        }
-
-             
-
-                  });
-
-                                     
-
-              }
-
-          }
-
-      }  
-
-   
-
-  
-
-  
-
-
+                        });
+                    }); 
+                }
+            });         
+        });
+    }  
 
   function onError(error) {
 
